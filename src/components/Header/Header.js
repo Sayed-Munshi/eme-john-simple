ae36437f1c8e1809b3d4c04486a2f25ff347a0e0
Sayed-Munshi <mdsayed3000@gmail.com>
2021-05-20 19:07:33 +0600

    secod commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <img src={logo} alt="" />
+
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/Order">Order Review</a>
+                <a href="/Manage">Manage Inventory here</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
